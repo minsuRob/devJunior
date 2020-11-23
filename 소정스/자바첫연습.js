fd2e982afa6d4bbca682a46b8f48866ba748b11a
소정 <ysj98827@naver.com>
2020-11-23 21:50:01 +0900

    자바스크립트1일차
    생활코딩 강의를 듣고 이론적인 이야기를 많이 해서 감을 익히기 위해 다른 자바스크립트 기초 영상을 보고
    어찌어찌 해봤다,,
    일단 html에 자바스크립트 연결 시키고, 자바 새파일을 만들어서 html console로 크로스 체크하며
    어떻게 입력 되고 있는지 확인 했다.
    데이터타입,, 산술연산자,, 변수와 대입연산자 등 사실 조금 어려워서
    자바 2~3일 따로 두드려 보고 진도를 나가야 될거 같다 ..!

---
+++ 소정스/자바첫연습.js
@@ -0,0 +1,11 @@
+// data-type
+
+//string, Number, Boolean, null, underfined
+const name = "윤소정";
+const age = 35;
+const hobbies = "계획짜기, 맛있는거 먹기, 공부"
+console.log("제 이름은 "+ name + "입니다. 제 나이는"+ age + "입니다.")
+
+
+console.log(name.substring(0.1))
+console.log(hobbies.split(",")[1])
