307c00d60a17c6f694e16fb9122018fb86b7c27e
minsu.rob <minsu.rob@gmail.com>
2020-12-24 23:00:10 +0900

    fix : Learning casting variable type and variable variables(가변변수)
    가변변수는 awesome 합니다

--- 민수쓰/syntaxForPHP/source/helloworld.php
+++ 민수쓰/syntaxForPHP/source/helloworld.php
@@ -1,38 +1,19 @@
 <html>
 <body>
-<?php
-$a=1;
-echo $a+1; #2
-echo "<br />";
-$a=2;
-print $a+1; #3
-?>
-
-<?php
-$first = "coding";
-echo $first." everybody";
-?>
 
 <?php
     $a = 100;
-    print $a.'<br />';
-    $a = $a + 10;
-    print $a.'<br />';
-    $a = $a / 10;
-    print $a.'<br />';
-    $a = $a - 10;
-    print $a.'<br />';
-    $a = $a * 10 ;    
-    print $a.'<br />';
-?>
-
+    echo gettype($a);
+    settype($a, 'double');
+    echo '<br />';
+    echo gettype($a);
+    ?>
 
 <?php
-print '<br />';
-define('TITLE', 'PHP Tutorial');
-echo TITLE;
-define('TITLE', 'JAVA Tutorial');
+$title = 'subject';
+$$title = 'PHP tutorial';
+echo '<br />';
+echo $subject;
 ?>
-
 </body>
 </html>
