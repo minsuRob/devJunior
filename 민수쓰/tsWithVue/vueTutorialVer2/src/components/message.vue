60596c392f5a5457960290d53b8ea00848de85a7
minsu.rob <minsu.rob@gmail.com>
2021-01-07 01:48:53 +0900

    (TS Chapter1) change project ts version 3 to 2.

---
+++ 민수쓰/tsWithVue/vueTutorialVer2/src/components/message.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <div>{{ message }}</div>
+  </div>
+</template> <script lang="ts">
+//1. ts명시
+import { Component, Vue } from "vue-property-decorator"; //2. import 
+@Component //3. Component 데코레이터 명시
+export default class Message extends Vue {
+  //4. 클래스 이름과 뷰 상속받기
+  message: string = "메세지를 입력해주세요";
+}
+</script> <style>
+</style>
+
---
+++ 민수쓰/vuehelloworld/src/components/message.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <div>{{ message }}</div>
+  </div>
+</template> <script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
