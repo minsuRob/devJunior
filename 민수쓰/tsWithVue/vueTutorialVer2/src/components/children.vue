169eda5de725c532a54ac5897e827adf50a07a44
minsu.rob <minsu.rob@gmail.com>
2021-01-07 23:38:18 +0900

    (Chapter2) parent에 존재하는 property 값들을 child들에게 적용하는 @Prop 확인

---
+++ 민수쓰/tsWithVue/vueTutorialVer2/src/components/children.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>{{ parentMessage }}</div>
+  </div>
+</template> <script lang="ts">
+//1. ts명시
+import { Component, Prop, Vue } from "vue-property-decorator"; //2. import 
+@Component 
+export default class Children extends Vue {
+  @Prop() parentMessage?:string;
+}
+</script> <style>
+</style>
+
--- 민수쓰/tsWithVue/vueTutorialVer2/src/views/Home.vue
+++ 민수쓰/tsWithVue/vueTutorialVer2/src/views/Home.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <message></message>
+    <children :parentMessage="message"></children>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import message from '@/components/message.vue'; // @ is an alias to /src
+import children from '@/components/children.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     message,
+    children,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  message:string = 'hello world';
+
+}
 </script>
