dbc86993cac21d1d00a7aafb18d8ef1264dd5742
Lee.D.Yeop <deabl19@naver.com>
2021-01-11 23:37:14 +0900

    PHP2
    변수의 중요성은 간단하게 많이 뿌려져있는 것을 간단하게 바꿀 수 있다는 점이 있다.

---
+++ 도엽스/php/var/var3.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<?php
+echo "변수의 중요성을 보여주마";
+echo "<br>";
+echo 100+10;
+echo '<br />';
+echo (100+10)/10;
+echo '<br />';
+echo ((100+10)/10)-10;
+echo '<br />';
+echo (((100+10)/10)-10)*10;
+echo '<br />';
+
+$a = 100;
+$a = $a + 10;
+print $a.'<br />';
+$a = $a / 10;
+print $a.'<br />';
+$a = $a - 10;
+print $a.'<br />';
+$a = $a * 10 ;    
+print $a.'<br />';
+?>
+</body>
+</html>
