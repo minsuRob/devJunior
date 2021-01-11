d311e1f65168097be9866dd9c905dcea27e77e69
Lee.D.Yeop <deabl19@naver.com>
2021-01-11 23:35:43 +0900

    php2
    따옴표를 꼭 시작하고 끝맺어줘야 문자로 인식한다. 안그러면 상수로 인식한다. 역슬레쉬 뒤에 따옴표가 오면 일시적으로 그 기능을 탈출 시켜준다.

---
+++ 도엽스/php/number_string/string.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hello world"; 
+echo "<br>";
+echo var_dump(1234); 
+echo "<br>";
+echo var_dump("1234"); 
+echo "<br>";
+echo "hello"." " ."world";
+echo "<br>"; 
+echo "그는 \"안녕하세요\" 라고 말했다";
+echo "<br>"; 
+echo "따옴표를 꼭 시작하고 끝맺어줘야 문자로 인식한다. 안그러면 상수로 인식한다.";
+echo "<br>"; 
+echo " 역슬레쉬 뒤에 따옴표가 오면 일시적으로 그 기능을 escaping 해준다. ";
+
+
+?>
