35577e90d6240e1934487f1b42866c2f8fa7a3a3
minsu.rob <minsu.rob@gmail.com>
2021-01-11 13:26:00 +0900

    (TS chpater3)Add provide inject event

--- 민수쓰/tsWithVue/vueTutorialVer2/src/components/children.vue
+++ 민수쓰/tsWithVue/vueTutorialVer2/src/components/children.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
     <div>
-      <p>{{ alertMsg }}</p>
-      {{ parentMessage }}
+      {{ message }}
     </div>
   </div>
 </template> <script lang="ts">
 //1. ts명시
-import { Component, Prop, Vue, Watch } from "vue-property-decorator"; //2. import
+import { Component, Prop, Vue, Watch, Emit, Model, Inject} from "vue-property-decorator"; //2. import
 @Component
 export default class Children extends Vue {
-  @Prop() parentMessage?: string;
-  alertMsg: string = '';
-  
-  @Watch('parentMessage', {immediate: false, deep: true})
-  update(val: string, oldVal : string) {
-    this.alertMsg = 'complete update msg'
-  }
+  @Inject() readonly message!: string;
 }
 </script> <style>
 </style>
--- 민수쓰/tsWithVue/vueTutorialVer2/src/views/Home.vue
+++ 민수쓰/tsWithVue/vueTutorialVer2/src/views/Home.vue
@@ -2,13 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <message></message>
-    <children :parentMessage="message"></children>
     <button @click="changeMsg">메세지 바꿔용</button>
+    <my-check-box v-model="checked" @change="change"></my-check-box>
+    {{text}}!<!---->
+    <children></children>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Provide } from "vue-property-decorator";
 import message from "@/components/message.vue"; // @ is an alias to /src
 import children from "@/components/children.vue"; // @ is an alias to /src
 
@@ -20,6 +22,11 @@ import children from "@/components/children.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {
   message: string = "hello world";
+  checked: boolean = false;
+  text:string = '동의X'
+
+  @Provide('message') msg: string = "provide/index sample";
+
   changeMsg() {
     this.message = "change";
   }
