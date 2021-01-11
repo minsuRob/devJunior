af8f9f996d5da026736d5d3e72f87e8b2585b15a
Lee.D.Yeop <deabl19@naver.com>
2021-01-11 23:36:16 +0900

    php2
    PHP에서 변수의 이름을 정의할 때 반드시 $ 4딸라 표시로 시작 해야한다.

---
+++ 도엽스/php/var/var1.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<?php
+echo " php 에서는 변수의 이름을 정의할 때 반드시 반드시 $ 표시로 시작 해야한다.";
+echo "<br>";
+
+$a=1;
+echo "<br>";
+echo $a;
+echo "<br>";
+echo $a+1;
+
+echo "<br>"; 
+
+$a=2;
+echo $a;
+echo "<br>";
+print $a+1;   //echo 와 print는 동일
+
+
+?>
+</body>
+</html>
