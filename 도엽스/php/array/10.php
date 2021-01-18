5528b09d74cba9c620bf99015fd710563465d48d
Lee.D.Yeop <deabl19@naver.com>
2021-01-18 23:37:57 +0900

    php 배열
    배열 안의 index를 추가 제거 할 수 있다.

---
+++ 도엽스/php/array/10.php
@@ -0,0 +1,13 @@
+<?php
+
+$li = ['a', 'b', 'c', 'd', 'e'];
+
+array_push ($li,'f'); //배열의 맨 뒤에 'f'라는 문자 추가
+
+array_unshift ($li,'z'); //배열의 맨 앞에 'z'라는 문자 추가
+
+array_shift ($li); //배열의 맨 앞에 문자 제거
+
+array_pop ($li); //배열의 맨 뒤에 문자 제거
+
+?>
