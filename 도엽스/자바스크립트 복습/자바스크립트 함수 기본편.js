c95a5419598f003e7ec21fe97c703e938c4b554e
Lee.D.Yeop <deabl19@naver.com>
2021-01-25 23:54:57 +0900

    자바스크립트 복습 1일차
    이해 안되는 부분을 다시 확인하기위해 복습 시작. 자바스크립트 복습 후 Vue.js 넘어갈 예정

---
+++ 도엽스/자바스크립트 복습/자바스크립트 함수 기본편.js
@@ -0,0 +1,18 @@
+
+
+function doSomething(add) {
+    console.log(add);
+    const result= add(2,3);
+    console.log(result);
+}
+
+
+
+funtion add(a,b) {
+    const sum = a*b;
+    return sum;
+}
+
+
+doSomething(add);
+
