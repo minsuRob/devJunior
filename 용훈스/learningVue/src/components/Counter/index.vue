81061cf41d657cf89c1d306e7d227a382ea5cacd
obeybook <obeybook@gmail.com>
2021-03-18 01:16:01 +0900

    feat: Counter 구현을 통해 Vue의 기본적인 상호작용 이해

---
+++ 용훈스/learningVue/src/components/Counter/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ number }}</p>
+    <button v-on:click="onIncrease">증가</button>
+    <button v-on:click="onDecrease">감소</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  methods: {
+    onIncrease() {
+      this.number++
+    },
+    onDecrease() {
+      this.number--
+    }
+  }
+};
+</script>
