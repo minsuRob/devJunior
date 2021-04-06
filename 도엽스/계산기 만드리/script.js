42adcb070fd90676328cf4dddd545e4734277941
Lee.D.Yeop <deabl19@naver.com>
2021-04-06 22:34:15 +0900

    자바스크립트 학습 24일차

--- 도엽스/계산기 만드리/script.js
+++ 도엽스/계산기 만드리/script.js
@@ -2,12 +2,37 @@ class Calculator {
   constructor(displayElement) {
     this.displayElement = displayElement;
     this.displayContent = "";
+    this.operatorCheck = true; // 연산자 선택 여부 저장
+    this.equalsCheck = false; // = 버튼 클릭 여부 관리
+    this.clear();
   }
   appendNumber(number) {
-    this.displayContent += number;
+    if (this.equalsCheck) {
+      this.displayContent = number;
+      this.equalsCheck = false;
+    } else {
+      this.displayContent += number;
+    }
+    this.operatorCheck = false; // 숫자 입력 시 false
+  }
+  appenOperator(operator) {
+    if (this.operatorCheck) return false; // operatorCheck값이 true면 함수 빠져나가기
+    this.displayContent += operator;
+    this.operatorCheck = true; // 연산자 입력 시 true
   }
   updateDisplay() {
     this.displayElement.value = this.displayContent;
+  }
+  clear() {
+    this.displayContent = "";
+    this.displayElement.value = 0;
+    this.operatorCheck = true;
+  }
+  compute() {
+    if (this.operatorCheck) return;
+    this.displayContent = eval(
+      this.displayContent.replace("\u00D7", "*").replace("\u00F7", "/") // 자바스크립트에서 해석할 수 있는 * / 로 변경
+    );
   }
 }
 
@@ -21,12 +46,17 @@ buttons.forEach((button) => {
     switch (button.dataset.type) {
       case "operator":
         console.log("operator");
+        calculator.appenOperator(button.innerText);
+        calculator.updateDisplay();
         break;
       case "ac":
         console.log("ac");
+        calculator.clear();
         break;
       case "equals":
         console.log("equals");
+        calculator.compute();
+        calculator.updateDisplay();
         break;
       default:
         calculator.appendNumber(button.innerText);
@@ -36,6 +66,4 @@ buttons.forEach((button) => {
   });
 });
 
-/*숫자 버튼을 클릭할 때마다 displayContent 속성에 숫자가 추가되고 input에도 표시되도록 appendNumber, updateDisplay 메서드 추가
-
-switch문의 default에서 추가한 메서드 호출 다음부터 진행 */
+//= 버튼을 클릭 후 숫자를 클릭한다면 AC기능과 비슷하게 새로운 식 입력을 시작하는거니
